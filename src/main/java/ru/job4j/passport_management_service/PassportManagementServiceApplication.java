d27c7667100df7be7a1dff0315bf71530dd8eb82
ultar <lester_g@inbox.ru>
2022-05-17 12:11:51 +0300

    Init commit

---
+++ src/main/java/ru/job4j/passport_management_service/PassportManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.job4j.passport_management_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassportManagementServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PassportManagementServiceApplication.class, args);
+    }
+
+}
